7c757a21e197aeb6a4f4fd277c12d2ad8325db20
Matt Talamantes <matthew@Matts-MacBook-Pro.local>
2019-06-14 16:56:20 -0700

    Added form with location submit that produces 10 results within 250 meters

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 // import './App.css';
 
 
+//Set the limit of results to 50 that way the results can be randomized and
+//shown to the user using math.random() and show/hide methods
+
 class App extends Component {
     constructor(props){
         super(props)
         this.state = {
             baseURL: 'https://api.foursquare.com/v2/',
-            search: 'venues/search?ll=40.7,-74',
+            search: 'venues/search?',
+            location: 'near=',
+            radius: '&radius=250',
             code: 200,
+            limit: '&limit=50',
             client_id: '&client_id=FAKQIEROR2HUJGJXALMHBCNMLM5HSIULTXO21UKSNS1IVN13',
             client_secret: '&client_secret=MEC1NRC3Z0EYTO41JTP5HCBYCBLSGNP3KHBY3P1INKWUPEBY',
-            query: '&query=sushi',
+            query: '&query=restaurant',
             v: '&v=20191231',
-            searchURL: ''
+            searchURL: '',
+            currentCity: ''
         }
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     //============================
@@ -24,13 +33,48 @@ class App extends Component {
     //https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=YYYYMMDD
 
 
+    //============================
+    //FOURSQUARE FOOD CATEGORY KEY
+    //============================
+
+    //4d4b7105d754a06374d81259
+
+
+
+    //=====================
+    //LOCATION INPUT METHOD
+    //=====================
+
+    handleSearch(event){
+        event.preventDefault()
+        console.log(this.state.currentCity);
+        let area = this.state.currentCity;
+        this.setState({
+            location: `near=${area}`
+        })
+    }
+
+
+    //====================
+    //HANDLE FORM CHANGE
+    //===================
+
+    handleChange(event) {
+       this.setState({
+         [event.target.id]: event.target.value
+       })
+    }
+
+
     //========================
     //FOURSQUARE FETCH METHOD
     //========================
+
+
     handleSubmit (event) {
       event.preventDefault()
       this.setState({
-        searchURL: this.state.baseURL + this.state.search + this.state.client_id + this.state.client_secret + this.state.v
+        searchURL: this.state.baseURL + this.state.search + this.state.location + this.state.radius + this.state.query + this.state.limit + this.state.client_id + this.state.client_secret + this.state.v
       }, () => {
         console.log(this.state.searchURL)
         fetch(this.state.searchURL)
@@ -46,6 +90,19 @@ class App extends Component {
             <div>
                 <h1>Pick-4-Me!</h1>
                 <button onClick={this.handleSubmit}>Get Data</button>
+                <div onSubmit={this.handleSearch}>
+                    <form >
+                        <label>Location: </label>
+                        <input
+                            id='currentCity'
+                            type='text'
+                            onChange={this.handleChange}
+                            placeholder='City, State'
+                            value={this.state.currentCity}
+                        />
+                        <button type='submit'>Submit</button>
+                    </form>
+                </div>
             </div>
         )
     }
